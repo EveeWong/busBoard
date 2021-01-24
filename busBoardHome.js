a52e10c11cc67d58ba2c896e938a932226d56749
Yvonne Wong <Yvonne.Wong@softwire.com>
2021-01-24 20:48:51 +0000

    Returns an array with the next 5 buses and the arrival times

--- busBoardHome.js
+++ busBoardHome.js
@@ -2,13 +2,29 @@ var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest
 
 var request = new XMLHttpRequest()
 
+function busFilter (busData) {
+    var busDataFilter = [];
+    for (let i = 0; i < busData.length; i++) {
+        //console.log(busData[i].lineId)
+        busDataFilter.push([busData[i].lineId, busData[i].expectedArrival])
+        };
+        console.log(busDataFilter)
+    }
+
 request.open('GET', 'https://api.tfl.gov.uk/StopPoint/490008660N/Arrivals', true)
 request.onload = function () {
     const busData = JSON.parse(request.responseText);
-    console.log(typeof(busData))
-    console.log(busData)
+    //console.log(typeof(busData))
+    //console.log(busData)
+
+    busFilter(busData);
+   
+            
 }
 
 request.send()
+
+
+
 
 
