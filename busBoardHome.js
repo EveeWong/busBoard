ed9172949c93121d0012a88d92ded7316e31eaaa
Yvonne Wong <Yvonne.Wong@softwire.com>
2021-01-24 23:49:40 +0000

    Refactored code for API request

--- busBoardHome.js
+++ busBoardHome.js
@@ -2,9 +2,10 @@ var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest
 
 var request = new XMLHttpRequest()
 
-// This function takes the orginal object an returns an array containing the ID, expected arrival time and time to arrival
+// This function takes the orginal object an returns an array containing the values
+// of the following properties ID, expected arrival time and time to arrival
 function busFilter (busData) {
-    var busDataFilter = [];
+    let busDataFilter = [];
     for (let i = 0; i < busData.length; i++) {
         //console.log(busData[i].lineId)
         busDataFilter.push([busData[i].lineId, busData[i].expectedArrival, busData[i].timeToStation])
@@ -15,7 +16,10 @@ function busFilter (busData) {
 
 
   //This part of the code makes the request to the API  
-    request.open('GET', 'https://api.tfl.gov.uk/StopPoint/490008660N/Arrivals', true)
+  var stopCode = "490008660N"
+  var URL = `https://api.tfl.gov.uk/StopPoint/${stopCode}/Arrivals`
+  function getBuses (method, URL) { 
+    request.open('GET', URL , true)
     request.onload = function () {
         //the requrest is returned as a string JSON.parse converts it into an array of objects
         const busData = JSON.parse(request.responseText);
@@ -25,7 +29,7 @@ function busFilter (busData) {
         const nextBuses = busFilter(busData)
     console.log(nextBuses)
     // const nextBuses is an array of arrays, each array containing the values of 3 properties
-    //which we filtered using the busFilter function defined above.
+    // which we filtered using the busFilter function defined above.
 
     
    
@@ -34,7 +38,7 @@ function busFilter (busData) {
 
 request.send()
 
+  }
 
-
-
+console.log(getBuses('GET', URL));
 
